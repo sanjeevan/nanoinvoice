2b97493027b6cf838735586bb043c34479c81a2e
Sanjeevan Ambalavanar <sanjeevan.a@gmail.com>
2013-06-16 11:26:26 +0100

    Migrations + models

---
+++ alembic/versions/006.py
@@ -0,0 +1,61 @@
+"""Subscriptions"""
+
+# revision identifiers, used by Alembic.
+revision = '006'
+down_revision = '005'
+
+import sqlalchemy as sa
+
+from alembic import op
+from sqlalchemy import func
+
+def upgrade():
+    op.create_table('plan',
+        sa.Column('id',                 sa.Integer(11),     nullable=False),
+        sa.Column('name',               sa.Unicode(20),     nullable=False),
+        sa.Column('description',        sa.Unicode(255),    nullable=False),
+        sa.Column('amount',             sa.Numeric(8, 2),   server_default='0.0'),
+        sa.Column('billing_interval',   sa.Enum('daily', 'weekly', 'monthly', 'yearly'), server_default=u'monthly'),
+
+        sa.Column('created_at', sa.DateTime, nullable=False),
+
+        sa.PrimaryKeyConstraint('id')
+    )
+
+    op.create_table('subscription',
+        sa.Column('id',         sa.Integer(11), nullable=False),
+        sa.Column('user_id',    sa.Integer(11), nullable=False),
+        sa.Column('plan_id',    sa.Integer(11), nullable=False),
+        sa.Column('start_date', sa.DateTime,    nullable=False),
+        sa.Column('end_date',   sa.DateTime,    nullable=True),
+        sa.Column('created_at', sa.DateTime,    nullable=False),
+        sa.Column('updated_at', sa.DateTime,    nullable=False, server_onupdate=func.now()),
+
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE' ),
+        sa.ForeignKeyConstraint(['plan_id'], ['plan.id'], ondelete='CASCADE' ),
+    )
+
+    op.create_table('transaction',
+        sa.Column('id',             sa.Integer(11), nullable=False),
+        sa.Column('user_id',        sa.Integer(11), nullable=False),
+        sa.Column('subscription_id',sa.Integer(11), nullable=False),
+        sa.Column('success',        sa.Boolean,     nullable=False, server_default=u'0'),
+        sa.Column('amount',         sa.Numeric(8, 2), default='0.0'),
+        sa.Column('charge_id',      sa.Unicode(100), nullable=True),
+        sa.Column('charge',         sa.UnicodeText(4294967295), nullable=True, default=u'{}'),
+
+        sa.Column('created_at', sa.DateTime, nullable=False),
+        sa.Column('updated_at', sa.DateTime, nullable=False, server_onupdate=func.now()),
+
+        sa.PrimaryKeyConstraint('id'),
+        sa.ForeignKeyConstraint(['user_id'], ['user.id'], ondelete='CASCADE' ),
+        sa.ForeignKeyConstraint(['subscription_id'], ['subscription.id'], ondelete='CASCADE' ),
+    )
+
+def downgrade():
+    op.drop_table('transaction')
+    op.drop_table('subscription')
+    op.drop_table('plan')
+
+
---
+++ nano/models/billing.py
@@ -0,0 +1,44 @@
+"""Application billing"""
+
+from datetime import datetime
+
+from nano.extensions import db
+
+class Plan(db.Model):
+    __tablename__ = 'plan'
+
+    id          = db.Column(db.Integer(11), primary_key=True, nullable=False)
+    name        = db.Column(db.Unicode(20))
+    description = db.Column(db.Unicode(255))
+    amount      = db.Column(db.Numeric(8, 2), default=0)
+    billing_interval = db.Column(db.Enum('daily', 'weekly', 'monthly', 'yearly'), default='monthly')
+
+    updated_at  = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    created_at  = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+class Subscription(db.Model):
+    __tablename__ = 'subscription'
+    id          = db.Column(db.Integer(11), primary_key=True, nullable=False)
+    user_id     = db.Column(db.Integer(11), db.ForeignKey('user.id'), nullable=False)
+    plan_id     = db.Column(db.Integer(11), db.ForeignKey('plan.id'), nullable=False)
+    start_date  = db.Column(db.DateTime, nullable=False)
+    end_date    = db.Column(db.DateTime, nullable=True)
+
+    updated_at  = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    created_at  = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+class Transaction(db.Model):
+    __tablename__ = 'transaction'
+
+    id              = db.Column(db.Integer(11), primary_key=True, nullable=False)
+    user_id         = db.Column(db.Integer(11), db.ForeignKey('user.id'), nullable=False)
+    subscription_id = db.Column(db.Integer(11), db.ForeignKey('subscription.id'), nullable=False)
+    success         = db.Column(db.Boolean, default=False)
+    amount          = db.Column(db.Numeric(8, 2), default=0)
+    charge_id       = db.Column(db.Unicode(100), nullable=True)
+    charge          = db.Column(db.UnicodeText(4294967295), nullable=True, default=u'{}')
+
+    updated_at  = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    created_at  = db.Column(db.DateTime, nullable=False, default=datetime.now, onupdate=datetime.now)
+
+
